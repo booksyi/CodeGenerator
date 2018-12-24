71b670e5ffe387046573c8ba200a54fc7e35b13a
Kai Huang <kaihaung@utsi.com.tw>
2018-12-24 20:47:00 +0800

    加入 ApiGeneratorController.cs

--- CodeGenerator/ClientApp/src/app/app.module.ts
+++ CodeGenerator/ClientApp/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { ApiGeneratorComponent } from './api-generator/api-generator.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
@@ -15,6 +16,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
+    ApiGeneratorComponent,
     CounterComponent,
     FetchDataComponent
   ],
@@ -24,6 +26,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'api-generator', component: ApiGeneratorComponent },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
