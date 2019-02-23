419dac8277630420dfc89656a6a83c9654f6f45d
kaihuang <kaihuang@utsi.com.tw>
2019-02-23 21:29:04 +0800

    加入 Migrations

---
+++ CodeGenerator/Migrations/20190223112322_Init.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeGenerator.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CodeGeneratorApiConstants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Result = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CodeGeneratorApiConstants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CodeGeneratorCodeTemplates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Node = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CodeGeneratorCodeTemplates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CodeGeneratorTemplates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 255, nullable: false),
+                    Description = table.Column<string>(maxLength: 500, nullable: true),
+                    Context = table.Column<string>(nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "(getdate())"),
+                    UpdateDate = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CodeGeneratorTemplates", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CodeGeneratorApiConstants");
+
+            migrationBuilder.DropTable(
+                name: "CodeGeneratorCodeTemplates");
+
+            migrationBuilder.DropTable(
+                name: "CodeGeneratorTemplates");
+        }
+    }
+}
